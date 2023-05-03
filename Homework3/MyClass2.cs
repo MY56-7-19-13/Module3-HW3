98cc1105c6c3256b8886db8b2c01f6bb9ff8472a
Maksym Yankovskyi <maks.yankovskiy27@gmail.com>
2023-05-03 14:29:22 +0300

    add homework3

---
+++ Homework3/MyClass2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    public delegate double DelegateForPow(double a, double b);
+    public delegate bool DelegateCalcHandler(double q);
+
+    public class MyClass2
+    {
+
+        double resultOfCalc;
+        public DelegateCalcHandler Calc(double a, double b)
+        {
+
+            MyClass1 myClass1 = new MyClass1();
+            DelegateForPow delegateForPow = myClass1.Pow;
+            resultOfCalc = delegateForPow.Invoke(a, b);
+            DelegateCalcHandler delegateCalcHandler = Result;
+            return delegateCalcHandler;
+
+        }
+        public bool Result(double number)
+        {
+            var resultOfResult = resultOfCalc % number;
+            if (resultOfResult == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
